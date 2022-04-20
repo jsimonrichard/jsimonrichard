7797721cf7242745e4c3f8f2f64d726d71adaa48
jsimonrichard <jsimonrichard@gmail.com>
2022-04-19 22:32:07 -0400

    Switch to Gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
+      options: {"plugins":[],"extensions":[".mdx"],"defaultLayouts":{},"gatsbyRemarkPlugins":[],"lessBabel":false,"remarkPlugins":[],"rehypePlugins":[],"mediaTypes":["text/markdown","text/x-markdown"],"root":"/home/jsimonrichard/Desktop/Resume/jsimonrichard","commonmark":false},
+    }]
---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `jsimonrichard-resume`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
