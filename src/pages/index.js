045a093749406daefce8e69cf431062a41629627
J. Simon Richard <jsimonrichard@gmail.com>
2023-07-29 12:52:28 -0400

    Hoverable images (with color)

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ const IndexPage = () => {
             id
             name
             childImageSharp {
-              gatsbyImageData(transformOptions: { grayscale: true })
+              gatsbyImageData
             }
           }
         }
@@ -398,7 +398,18 @@ const IndexPage = () => {
                   align-items: center;
                 `}
               >
-                <a href={skillsURLs[name]} target="_blank" rel="noreferrer">
+                <a href={skillsURLs[name]} target="_blank" rel="noreferrer" css={css`
+                  -webkit-filter: grayscale(100%);
+                  -moz-filter: grayscale(100%);
+                  filter: grayscale(100%);
+                  transition: all 0.5s ease;
+
+                  :hover {
+                    -webkit-filter: grayscale(0%);
+                    -moz-filter: grayscale(0%);
+                    filter: grayscale(0%);
+                  }
+                `}>
                   <GatsbyImage image={gatsbyImageData} alt={name} />
                 </a>
               </div>
