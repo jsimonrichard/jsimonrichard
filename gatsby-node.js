8730199ec2a43d6c18212bfb5157fd4660ccf7f7
J. Simon Richard <jsimonrichard@gmail.com>
2024-06-29 12:30:53 -0400

    Correct file extensions

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
-const _ = require('lodash');
-const path = require('path');
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const _ = require("lodash");
+const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // Update blog slugs
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
   // Ensures we are processing only markdown files
   if (node.internal.type === "Mdx") {
     // Get relative path for files on disk
@@ -12,27 +12,27 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       node,
       getNode,
       basePath: "blog/",
-    })
+    });
 
     // Creates new query'able field with name of 'slug'
     createNodeField({
       node,
       name: "slug",
       value: `blog${relativeFilePath}`,
-    })
+    });
   }
-}
+};
 
 // Create extra content
-exports.createPages = async ({actions, graphql}) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
 
-  const postTemplate = path.resolve('./src/templates/post-template.js');
-  const tagTemplate = path.resolve(`./src/templates/tags-page-template.js`);
+  const postTemplate = path.resolve("./src/templates/post-template.jsx");
+  const tagTemplate = path.resolve(`./src/templates/tags-page-template.jsx`);
 
   const result = await graphql(`
     query Posts {
-      allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+      allMdx(sort: { order: DESC, fields: frontmatter___date }) {
         nodes {
           fields {
             slug
@@ -42,7 +42,7 @@ exports.createPages = async ({actions, graphql}) => {
             tags
           }
         }
-      },
+      }
       allFile(filter: { extension: { eq: "pdf" } }) {
         edges {
           node {
@@ -53,31 +53,31 @@ exports.createPages = async ({actions, graphql}) => {
       }
     }
   `);
-  
+
   const posts = result.data.allMdx.nodes;
 
   // Create posts
-  posts.forEach(node => {
-    console.log("Creating page for", node)
+  posts.forEach((node) => {
+    console.log("Creating page for", node);
     createPage({
       path: node.fields.slug,
-      component: postTemplate,//+"?__contentFilePath="+node.fileAbsolutePath,
+      component: postTemplate, //+"?__contentFilePath="+node.fileAbsolutePath,
       context: {
-        slug: node.fields.slug
-      }
-    })
-  })
+        slug: node.fields.slug,
+      },
+    });
+  });
 
   // Create tag pages
   let tags = [];
-  posts.forEach(post => {
+  posts.forEach((post) => {
     tags = tags.concat(post.frontmatter.tags);
   });
-  _.uniq(tags).forEach(tag => {
+  _.uniq(tags).forEach((tag) => {
     createPage({
       path: `/blog/tags/${tag}`,
       component: tagTemplate,
-      context: { tag }
+      context: { tag },
     });
   });
 };
